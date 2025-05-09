9e5ddba8ccf85b0e530487f56800622812fa8ed4
David Mort <david_mort@waters.com>
2025-05-09 13:03:44 +0100

    update2

--- run_service.sh
+++ run_service.sh
@@ -4,10 +4,10 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # Activate virtual environment
-source "$SCRIPT_DIR/.venv/bin/activate"
+source "$SCRIPT_DIR/venv/bin/activate"
 
 # Set the working directory to the project root
 cd "$SCRIPT_DIR"
 
 # Run the FastAPI application using uvicorn with full path
-exec "$SCRIPT_DIR/.venv/bin/uvicorn" app.main:app --host 0.0.0.0 --port 8000 --workers 1 
+exec "$SCRIPT_DIR/venv/bin/uvicorn" app.main:app --host 0.0.0.0 --port 8000 --workers 1 
